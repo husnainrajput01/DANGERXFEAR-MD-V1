cf3abd16f3e1df3fdf33c817096a6629cdfb617d
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2026-01-13 10:13:48 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝕯𝖆𝖓𝖌𝖊𝖗𝕏ꜰᴇᴀʀ Bot',
+  author: '‎',
+  botName: "𝕯𝖆𝖓𝖌𝖊𝖗𝕏ꜰᴇᴀʀ Bot",
+  botOwner: '𝕯𝕏ꜰ(HUSNAIN)', // Your name
+  ownerNumber: '923021728750', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/husnainrajput01/DANGERXFEAR-MD-V1/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
